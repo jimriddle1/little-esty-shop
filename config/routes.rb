fffaa954505b9f992f964ef71f771d97c90b937b
Matt Deming <vballmatt44@gmail.com>
2022-06-15 08:18:39 -0600

    Try to troubleshoot heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "application#index"
   get '/admin', to: 'admins#dashboard'
 
   resources :admin_merchants, except: [:destroy]
